0ea29d3d794b9f5b565fb4b9db6723e388dc8b1b
Yulmukhametov <almaz.yulmukhametov@gmail.com>
2022-05-14 15:46:05 +0500

    Попытка сделать 4ую лабораторную работу

--- src/main/java/team3/Calc.java
+++ src/main/java/team3/Calc.java
@@ -1,5 +1,49 @@
 package team3;
 
-public class Calc {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="Calc", urlPatterns="/JavaCalc")
+public class Calc extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestCalc Calc = RequestCalc.fromRequestParameters(request);
+        Calc.setAsRequestAttributesAndCalculate(request);
+
+        request.getRequestDispatcher("/Results.jsp").forward(request, response);
+
+    }
+
+    private static class RequestCalc {
+        private final String f_calc;
+        private double result;
+
+        private RequestCalc (String first) {
+            this.f_calc = first;
+        }
+
+        public static RequestCalc fromRequestParameters(HttpServletRequest request) {
+            return new RequestCalc(request.getParameter("rad"));
+        }
+
+        public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
+            request.setAttribute("first_result", f_calc);
+            double rad;
+            try {
+                rad=Double.parseDouble(f_calc);
+            }
+            catch (NumberFormatException e) {
+                rad=0;
+            }
+
+            result=rad*rad*Math.PI;
+            request.setAttribute("result", result);
+        }
+    }
 
 }
--- src/main/webapp/Result.jsp
+++ src/main/webapp/Result.jsp
@@ -3,10 +3,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+    <meta charset="UTF-8">
+    <title>Результат</title>
 </head>
 <body>
-
+        <h1>Площадь круга</h1>
+            <h3>Радиус: ${f_result}</h3>
+            <h3>Результат: ${result}</h3>
+        <form action="${pageContext.request.contextPath}/Form.jsp">
+        </form>
 </body>
 </html>
